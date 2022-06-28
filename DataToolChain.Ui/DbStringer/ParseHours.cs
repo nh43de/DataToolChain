7cb57e85c98d23452ac24b74083bc95e7b86af4a
Nathan Hollis <natehollis@gmail.com>
2022-06-28 12:09:56 -0500

    Fix

--- DataToolChain.Ui/DbStringer/ParseHours.cs
+++ DataToolChain.Ui/DbStringer/ParseHours.cs
@@ -9,7 +9,7 @@ namespace DataToolChain.Ui.DbStringer
     {
         //
         //private static readonly Regex HoursRegex = new Regex(@"(<hour>[0-9]{1,2}):?(?<minute>[0-9]{2})?\W*(?<tod>am|pm)?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        private static readonly Regex HoursRegex = new Regex(@"(?<hour1>[0-9]{1,2}):?(?<minute1>[0-9]{2})?\s*(?<tod1>am|pm)?\s*-\s*(?<hour2>[0-9]{1,2}?):?(?<minute2>[0-9]{2})?\s*(?<tod2>am|pm)?($|\W)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex HoursRegex = new Regex(@"(?<preText>.*?)(?<hour1>[0-9]{1,2}):?(?<minute1>[0-9]{2})?\s*(?<tod1>am|pm)?\s*-\s*(?<hour2>[0-9]{1,2}?):?(?<minute2>[0-9]{2})?\s*(?<tod2>am|pm)?($|\W)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         
         public static string Parse(string timeStrings)
         {
@@ -52,7 +52,9 @@ namespace DataToolChain.Ui.DbStringer
 
                 var tsDiff = tsEnd - tsStart;
 
-                var r = $"{tsStart.Hours}:{tsStart.Minutes:D2}-{tsEnd.Hours}:{tsEnd.Minutes:D2} \t{Math.Round(tsDiff.TotalHours, 2)}\thours";
+                var preText = match1[0]?.Groups["preText"]?.Value;
+
+                var r = $"{preText}{tsStart.Hours}:{tsStart.Minutes:D2}-{tsEnd.Hours}:{tsEnd.Minutes:D2} \t{Math.Round(tsDiff.TotalHours, 2)}\thours";
 
                 return r;
             });
